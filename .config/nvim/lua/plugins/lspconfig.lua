3dc9a9f296262395596a537ad053f0628f726ef8
Brandon Lucier <brandon.lucier@agilebits.com>
2024-09-09 13:04:12 -0400

    install typescript LSP

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -10,7 +10,8 @@ return {
       opts = {
         ensure_installed = {
             'rust_analyzer',
-            'lua_ls'
+            'lua_ls',
+            'ts_ls',
         }
       }
     },
@@ -24,9 +25,32 @@ return {
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
         lspconfig['rust_analyzer'].setup {
-            capabilities = capabilities
+            capabilities = capabilities,
+            settings = {
+                ['rust-anayzer'] = {
+                    rust = {
+                      analyzerTargetDir = true,
+                    },
+                    checkOnSave = {
+                      command = 'clippy',
+                    },
+                    files = {
+                      excludeDirs = {
+                          '.git',
+                          '.direnv',
+                          'target',
+                          'js',
+                          'node_modules',
+                      },
+                    },
+                },
+            },
+
         }
         lspconfig['lua_ls'].setup {
+            capabilities = capabilities
+        }
+        lspconfig['ts_ls'].setup {
             capabilities = capabilities
         }
       end
