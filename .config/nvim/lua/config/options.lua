edcb4914482d7a633ad0645b2fd27f8ef313031e
Scetch <me@scet.ch>
2024-09-22 10:41:37 -0400

    feat(nvim): add keymap for LSP formatting

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,6 +7,7 @@ vim.diagnostic.config {
 
 vim.keymap.set('n', '<leader>p', '"+p')
 vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('n', '<leader>s', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>')
 
 opt.number = true
 
