49564e053ad2ca8882b96564f77c8f60372949af
Scetch <lucierbrandon@gmail.com>
2025-04-16 13:37:26 -0400

    Changed line wrap, added noh to Esc, added scrolloff

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,25 +1,22 @@
-local opt = vim.opt
-
 vim.g.mapleader = ' '
 vim.diagnostic.config {
     update_in_insert = true
 }
 
-vim.keymap.set('n', '<leader>p', '"+p')
-vim.keymap.set('n', '<leader>P', '"+P')
 vim.keymap.set('n', '<leader>s', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>')
 vim.keymap.set('n', '<leader>c', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>');
 
-opt.number = true
+vim.opt.number = true
 
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.autoindent = true
-
-opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 opt.cursorline = true
 opt.termguicolors = true
 
-opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.scrolloff = 10
